10a1fa393e08effc06f2ef54b93a7119b7858e6f
JoseAlba <josealba1996@hotmail.com>
2016-09-17 19:15:55 -0400

    Changed Files

---
+++ PhpstormProjects/colours/js/controller.js
@@ -0,0 +1,8 @@
+angular.module('radioExample', [])
+    .controller('ExampleController', ['$scope', function($scope) {
+        $scope.function = {
+            name: 'blue'
+        };
+
+        $scope.
+    }]);
